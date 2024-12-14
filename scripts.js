bd99cd66f8a4819cc831c86508e7fba681e02875
KaueSverberi <miletkaue@gmail.com>
2024-12-13 22:22:51 -0300

    Primeiro commit site Conectamente - startup feita para trabalho escolar

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const isaac = document.querySelector(".ver-mais-isaac")
+const victor = document.querySelector(".ver-mais-victor")
+const kaue = document.querySelector(".ver-mais-kaue")
+const claudio = document.querySelector(".ver-mais-claudio")
+const pedro = document.querySelector(".ver-mais-pedro")
+const mascara = document.querySelector(".mascara-ver-mais")
+
+function cliqueinoIsaac() {
+    isaac.style.left = "50%";
+    isaac.style.transform = "translateX(50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinoVictor() {
+    victor.style.left = "50%";
+    victor.style.transform = "translateX(50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinoKaue() {
+    kaue.style.left = "50%";
+    kaue.style.transform = "translateX(50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinoClaudio() {
+    claudio.style.left = "50%";
+    claudio.style.transform = "translateX(50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinoPedro() {
+    pedro.style.left = "50%";
+    pedro.style.transform = "translateX(50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinoFechar() {
+    isaac.style.left = "-600px"
+    isaac.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
+
+function cliqueinoFecharvictor() {
+    victor.style.left = "-600px"
+    victor.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
+
+function cliqueinoFecharkaue() {
+    kaue.style.left = "-600px"
+    kaue.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
+
+function cliqueinoFecharclaudio() {
+    claudio.style.left = "-600px"
+    claudio.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
+
+function cliqueinoFecharpedro() {
+    pedro.style.left = "-600px"
+    pedro.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
